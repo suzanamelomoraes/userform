f793ef3b7a39de8c650580e1a2134df0e6604a4b
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-12 22:21:16 +1300

    create function to validate name input

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ document.getElementById("name").addEventListener("blur", validateName);
 document.getElementById("zip").addEventListener("blur", validateZip);
 document.getElementById("email").addEventListener("blur", validateEmail);
 document.getElementById("phone").addEventListener("blur", validatePhone);
+
+function validateName() {
+  const name = document.getElementById("name");
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if (!re.test(name.value)) {
+    name.classList.add("is-invalid");
+  } else {
+    name.classList.remove("is-invalid");
+  }
+}
