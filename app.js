8f9b42b4f7735ecfbf7cc62582150b4733a63cd6
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-12 22:02:55 +1300

    get elements from the form by ID and add event listeners

---
+++ app.js
@@ -0,0 +1,6 @@
+//Form blur Event Listeners
+
+document.getElementById("name").addEventListener("blur", validateName);
+document.getElementById("zip").addEventListener("blur", validateZip);
+document.getElementById("email").addEventListener("blur", validateEmail);
+document.getElementById("phone").addEventListener("blur", validatePhone);
